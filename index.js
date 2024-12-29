878e3274f8fd12f9c6a0ff50ad2735d54877cb35
Kalin Konstantinov <kalink.wow@gmail.com>
2024-12-29 03:22:09 +0100

    add loading logic for the document

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 import 'dotenv/config';
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
-console.log(process.env.GROG_API_KEY);
+const loader = new PDFLoader("./nke-10k-2023.pdf");
+
+const docs = await loader.load();
+console.log(docs.length);
