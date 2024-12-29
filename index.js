cbf666731c8e04413b6b1f9ed5f0573af69b2aee
Kalin Konstantinov <kalink.wow@gmail.com>
2024-12-29 03:30:30 +0100

    split the docs

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import 'dotenv/config';
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
 
 const loader = new PDFLoader("./nke-10k-2023.pdf");
 
 const docs = await loader.load();
-console.log(docs.length);
+
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 1000,
+  chunkOverlap: 200,
+});
+
+const allSplits = await textSplitter.splitDocuments(docs);
+
+console.log(allSplits.length);
